646aab71f63653a8d559a6d733d084157b7e2690
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-09-20 11:32:20 -0500

    Bugfix?

--- runSysCheck.sh
+++ runSysCheck.sh
@@ -5,15 +5,15 @@ lspci | grep VGA | sed -e 's/.*://' | xargs \
 lscpu | grep 'Model name' | sed -e 's/.*://' -e 's/([^()]*)//g' | xargs \
 	echo "CPU           :"
 BATTERY=`upower -e | grep battery | xargs upower -i | grep capacity | sed -e 's/.*://' -e 's/%//' ` 
-if [ $BATTERY -le "50" ]
+if [ $BATTERY -le 50 ]
 then
 	echo "Battery Health: Poor (Dispose)"
 	exit 1;
-elif [ $BATTERY -le "80" ]
+elif [ $BATTERY -le 80 ]
 then
 	echo "Battery Health: Moderate (Grade B)"
 	exit 1;
-elif [ $BATTERY -gt "80" ]
+elif [ $BATTERY -gt 80 ]
 then
 	echo "Battery Health: Good (Grade A)"
 	exit 1;
