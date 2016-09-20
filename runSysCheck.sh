b34c1912bd3855f56302bc764cb7bd24a956e178
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-09-20 11:25:47 -0500

    Formatting, battery grading according to the following:
    <50% - Dispose
    50-80% - Grade B
    80+% - Grade A

--- runSysCheck.sh
+++ runSysCheck.sh
@@ -1,4 +1,23 @@
-xdpyinfo | grep dimensions | sed -e 's/dimensions://g' -e 's/([^()]*)//g' -e 's/pixels//g' | xargs echo "Resolution:"
-lspci | grep VGA | sed -e 's/.*://' | xargs echo "Graphics:"
-lscpu | grep 'Model name' | sed -e 's/.*://' | xargs echo "CPU:"
-upower -e | grep battery | xargs upower -i | grep capacity | sed -e 's/.*://' | xargs echo "Battery Health:"
+xdpyinfo | grep dimensions | sed -e 's/dimensions://g' -e 's/([^()]*)//g' -e 's/pixels//g' | xargs \
+	echo "Resolution    :"
+lspci | grep VGA | sed -e 's/.*://' | xargs \
+	echo "Graphics      :"
+lscpu | grep 'Model name' | sed -e 's/.*://' -e 's/([^()]*)//g' | xargs \
+	echo "CPU           :"
+BATTERY=`upower -e | grep battery | xargs upower -i | grep capacity | sed -e 's/.*://' -e 's/%//' ` 
+if [ $BATTERY -le "50" ]
+then
+	echo "Battery Health: Poor (Dispose)"
+	exit 1;
+elif [ $BATTERY -le "80" ]
+then
+	echo "Battery Health: Moderate (Grade B)"
+	exit 1;
+elif [ $BATTERY -gt "80" ]
+then
+	echo "Battery Health: Good (Grade A)"
+	exit 1;
+else
+	echo "There was an error, battery health is at " $BATTERY
+	exit 1;
+fi
