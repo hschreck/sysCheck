ab5c10290d02acc72b42b19a60a9b75cf24599b5
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-09-20 09:49:57 -0500

    Formatted to look nicer and more readable

--- runSysCheck.sh
+++ runSysCheck.sh
@@ -1,4 +1,4 @@
-xdpyinfo | grep dimensions
-lspci | grep VGA
-lscpu | grep 'Model name'
-upower -e | grep battery | xargs upower -i | grep capacity
+xdpyinfo | grep dimensions | sed -e 's/dimensions://g' -e 's/([^()]*)//g' -e 's/pixels//g' | xargs echo "Resolution:"
+lspci | grep VGA | sed -e 's/.*://' | xargs echo "Graphics:"
+lscpu | grep 'Model name' | sed -e 's/.*://' | xargs echo "CPU:"
+upower -e | grep battery | xargs upower -i | grep capacity | sed -e 's/.*://' | xargs echo "Battery Health:"
