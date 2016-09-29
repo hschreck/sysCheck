ac6773c30fe500400f1e869bc68d356f95bca1bf
Harold Schreckengost <harold@haroldmschreckengost.com>
2016-09-29 13:53:47 -0500

    Fixed an issue with batteries; relies on something of a horrid little hack to fix the fact that some batteries simply do not report their status correctly

--- runSysCheck.sh
+++ runSysCheck.sh
@@ -4,20 +4,5 @@ lspci | grep VGA | sed -e 's/.*://' | xargs \
 	echo "Graphics      :"
 lscpu | grep 'Model name' | sed -e 's/.*://' -e 's/([^()]*)//g' | xargs \
 	echo "CPU           :"
-BATTERY=`upower -e | grep battery | xargs upower -i | grep capacity | sed -e 's/.*://' -e 's/%//' -e 's/\..*$//' ` 
-if [ "$BATTERY" -le 50 ]
-then
-	echo "Battery Health: Poor (Dispose)" $BATTERY
-	exit 1;
-elif [ "$BATTERY" -le 80 ]
-then
-	echo "Battery Health: Moderate (Grade B)" $BATTERY
-	exit 1;
-elif [ "$BATTERY" -gt 80 ]
-then
-	echo "Battery Health: Good (Grade A)" $BATTERY
-	exit 1;
-else
-	echo "There was an error, battery health is at " $BATTERY
-	exit 1;
-fi
+
+python syscheck.py
---
+++ syscheck.py
@@ -0,0 +1,36 @@
+import os
+
+workaroundModels= [
+        'HP EliteBook 2540p',
+        ]
+
+health_low=50
+health_mid=80
+
+designcap = os.popen("sudo dmidecode --type 22 | grep 'Design Capacity' | sed 's/[^0-9]*//g' ").read()
+
+designvolt = os.popen("sudo dmidecode --type 22 | grep 'Design Voltage' | sed 's/[^0-9]*//g' ").read()
+
+currentcap = os.popen("upower -e | grep battery | xargs upower -i | grep energy-full: | sed -e 's/ Wh//' -e 's/.* //' ").read()
+
+modelName = os.popen("sudo dmidecode --type 1 | grep 'Product Name' | sed 's/.*: //g' ").read()
+
+if modelName in workaroundModels:
+    designcap = float(designcap) * float(designvolt) / 1000000
+else:
+    designcap = float(designcap) / 1000
+
+currentcap=float(currentcap)
+#print(designcap)
+#print(currentcap)
+health = round((currentcap/designcap) * 100, 2)
+
+if health < health_low :
+    print("Battery       : " + str(health) + "% (Dispose)")
+elif health < health_mid :
+    print("Battery       : " + str(health) + "% (Grade B)")
+elif health > health_mid :
+    print("Battery       : " + str(health) + "% (Grade A)")
+else:
+    print("Battery not detected or is dead.")
+
